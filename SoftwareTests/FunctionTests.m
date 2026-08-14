44cd21a7e84cb1d6342f7e528ea84ead6ddfce5a
Wynter Duncanson <wduncans@mathworks.com>
2026-08-14 10:07:59 -0400

    Minor updates

--- SoftwareTests/FunctionTests.m
+++ SoftwareTests/FunctionTests.m
@@ -1,8 +1,13 @@
 classdef FunctionTests < matlab.unittest.TestCase
+    % FunctionTests - Placeholder test class for functions in development
+    %
+    % This class serves as a template for writing unit tests for any functions
+    % being developed in the FunctionLibrary folder. Add test methods here to
+    % validate the behavior of new functions as they are created and refined.
+    %
+    % Reference: https://www.mathworks.com/help/matlab/matlab_prog/use-parameters-in-class-based-tests.html
 
-    % https://www.mathworks.com/help/matlab/matlab_prog/use-parameters-in-class-based-tests.html
-
-    methods(Test)    
+    methods(Test)
 
     end % methods
 
